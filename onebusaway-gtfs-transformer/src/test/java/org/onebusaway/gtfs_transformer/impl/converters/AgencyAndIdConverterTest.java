20175fe5eb46bcb93547eebca8e665cfbe3420b5
Brian Ferris <bdferris@google.com>
2011-08-17 20:39:49 +0000

    Temporarily disable the plugin while we wait for our patch to maven-license-plugin more flexible header support to (hopefully) be accepted

---
+++ onebusaway-gtfs-transformer/src/test/java/org/onebusaway/gtfs_transformer/impl/converters/AgencyAndIdConverterTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.gtfs_transformer.impl.converters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.onebusaway.gtfs.model.AgencyAndId;
+
+public class AgencyAndIdConverterTest {
+
+  @Test
+  public void testConvertFromString() {
+    AgencyAndIdConverter converter = new AgencyAndIdConverter();
+    AgencyAndId id = (AgencyAndId) converter.convert(AgencyAndId.class, "a_b");
+    assertEquals("a", id.getAgencyId());
+    assertEquals("b", id.getId());
+  }
+
+  @Test
+  public void testConvertToString() {
+    AgencyAndIdConverter converter = new AgencyAndIdConverter();
+    AgencyAndId id = new AgencyAndId("a", "b");
+    String value = (String) converter.convert(String.class, id);
+    assertEquals("a_b", value);
+  }
+
+  @Test
+  public void testNull() {
+    AgencyAndIdConverter converter = new AgencyAndIdConverter();
+    Object value = converter.convert(AgencyAndId.class, null);
+    assertNull(value);
+  }
+}

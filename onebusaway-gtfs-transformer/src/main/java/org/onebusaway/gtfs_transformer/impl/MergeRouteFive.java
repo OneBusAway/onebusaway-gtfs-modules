1530aba6326a14fe0ff9d2674f3adce62ef90dcc
heidebritta <heidebritta@gmail.com>
2018-04-16 12:24:58 -0400

    MOTP-687 Merge Subway 5 and 5X. Route 5X trips mapped to Route 5.  Route 5X removed.

---
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/MergeRouteFive.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2018 Cambridge Systematics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onebusaway.gtfs_transformer.impl;
+
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.gtfs.model.Route;
+import org.onebusaway.gtfs.model.Trip;
+import org.onebusaway.gtfs.services.GtfsMutableRelationalDao;
+import org.onebusaway.gtfs_transformer.services.GtfsTransformStrategy;
+import org.onebusaway.gtfs_transformer.services.TransformContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
+public class MergeRouteFive implements GtfsTransformStrategy {
+
+    private final Logger _log = LoggerFactory.getLogger(MergeRouteFromReferenceStrategy.class);
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public void run(TransformContext context, GtfsMutableRelationalDao dao) {
+        RemoveEntityLibrary removeEntityLibrary = new RemoveEntityLibrary();
+
+        String agency = dao.getAllTrips().iterator().next().getId().getAgencyId();
+
+        //Merge route 5X into route 5, then remove route 5X
+
+        Route routeFive = dao.getRouteForId(new AgencyAndId(agency, "5"));
+        Route routeFiveX = dao.getRouteForId(new AgencyAndId(agency, "5X"));
+
+        for (Trip trip : dao.getTripsForRoute(routeFiveX)) {
+            trip.setRoute(routeFive);
+        }
+        dao.removeEntity(routeFiveX);
+    }
+}
+

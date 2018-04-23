363df7dac9afdb0c36186aeaf7ebebb86e514e9d
heidebritta <heidebritta@gmail.com>
2018-04-23 19:33:41 -0400

    MOTP-715 Routes 2/3 not serving Penn Station after Transformations.  There are 12 stops that get removed as duplicates.  The stop times were not properly assigned from the duplicate that gets removed to the duplicate that persists.

--- onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/UpdateStopIdFromControlStrategy.java
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/UpdateStopIdFromControlStrategy.java
@@ -18,6 +18,7 @@ package org.onebusaway.gtfs_transformer.impl;
 import org.onebusaway.csv_entities.schema.annotations.CsvField;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.gtfs.model.Stop;
+import org.onebusaway.gtfs.model.StopTime;
 import org.onebusaway.gtfs.services.GtfsMutableRelationalDao;
 import org.onebusaway.gtfs_transformer.services.GtfsTransformStrategy;
 import org.onebusaway.gtfs_transformer.services.TransformContext;
@@ -53,9 +54,12 @@ public class UpdateStopIdFromControlStrategy implements GtfsTransformStrategy {
         int matched = 0;
         int unmatched = 0;
         int duplicate = 0;
+        int inCntrlRefNotAtis = 0;
 
-        ArrayList<AgencyAndId> stopsAdded = new ArrayList();
         ArrayList<AgencyAndId> stopsToRemove = new ArrayList();
+        //a map of the new id (from reference/control file) to the old id
+        //so that stop times can be associated with new id
+        HashMap<AgencyAndId, AgencyAndId> stopsAdded = new HashMap<>();
 
         HashMap<String, Stop> referenceStops = new HashMap<>();
         for (Stop stop : reference.getAllStops()) {
@@ -63,7 +67,6 @@ public class UpdateStopIdFromControlStrategy implements GtfsTransformStrategy {
         }
 
         AgencyAndId agencyAndId = dao.getAllStops().iterator().next().getId();
-        _log.info("Stop Agency: {}, dao AgencyAndId: {}", agencyAndId.getAgencyId(), getDaoAgencyId(dao));
 
         for (String controlLine : controlLines) {
             String[] controlArray = controlLine.split(",");
@@ -79,47 +82,61 @@ public class UpdateStopIdFromControlStrategy implements GtfsTransformStrategy {
             if (direction.isEmpty()) {
                 refStop = referenceStops.get(referenceId);
                 if (refStop == null) {
-                    _log.info("missing reference stop {} for agency {}", referenceId, getReferenceAgencyId(reference));
+                    if (!atisId.equals("0")) {
+                        //_log.info("missing reference stop {} for agency {} for ATIS id {}", referenceId, getReferenceAgencyId(reference), atisId);
+                    }
                     unmatched++;
                     continue;
                 }
             } else {
                 refStop = referenceStops.get(referenceId+direction);
                 if (refStop == null) {
-                    _log.info("missing reference stop {} for agency {}", referenceId+direction, getReferenceAgencyId(reference));
+                    if (!atisId.equals("0")) {
+                        //_log.info("missing reference stop {} for agency {} for ATIS id {}", referenceId, getReferenceAgencyId(reference), atisId);
+                    }
                     unmatched++;
                     continue;
                 }
             }
+            Stop atisStop = dao.getStopForId(new AgencyAndId(agencyAndId.getAgencyId(), atisId));
             //don't add duplicates
             //if the reference id already exists as a stop, skip
-            if (stopsAdded.contains(refStop.getId())){
-                _log.info("Duplicate stop: " + refStop.getId());
+            if (stopsAdded.containsKey(refStop.getId())) {
                 duplicate++;
+                Stop persistStop = dao.getStopForId(stopsAdded.get(refStop.getId()));
                 stopsToRemove.add(new AgencyAndId(agencyAndId.getAgencyId(), atisId));
+                //reassign all the stop_times from this stop to the one that persists
+                //need to use the original stop id as the 'new' one isn't saved in dao (yet?)
+                for (StopTime stopTime : dao.getStopTimesForStop(atisStop)) {
+                    stopTime.setStop(persistStop);
+                }
+                _log.info("Duplicate stops keep: {}  remove: {} ", persistStop.getId().getId(), atisStop.getId().getId());
                 continue;
             }
 
-            Stop atisStop = dao.getStopForId(new AgencyAndId(agencyAndId.getAgencyId(), atisId));
+
             if (atisStop == null) {
-                _log.info("missing atis stop {} for agency {}", atisId, agencyAndId.getAgencyId());
+                if (!atisId.equals("0")) {
+                    //_log.info("missing atis stop {} for Reference id {}{}", atisId, referenceId, direction);
+                }
                 unmatched++;
                 continue;
             }
+
             matched++;
             atisStop.setName(refStop.getName());
             atisStop.setDirection(refStop.getDirection());
             atisStop.setId(refStop.getId());
             atisStop.setParentStation(refStop.getParentStation());
             atisStop.setLocationType(refStop.getLocationType());
-            stopsAdded.add(atisStop.getId());
+            stopsAdded.put(atisStop.getId(), new AgencyAndId(agencyAndId.getAgencyId(), atisId));
         }
         _log.info("Complete with {} matched and {} unmatched and {} duplicates", matched, unmatched, duplicate);
 
         for (AgencyAndId id : stopsToRemove) {
-            _log.info("Removing stop: " + id);
             Stop stop = dao.getStopForId(id);
-            removeEntityLibrary.removeStop(dao, stop);
+            //removeEntityLibrary.removeStop(dao, stop);
+            dao.removeEntity(stop);
         }
     }
 

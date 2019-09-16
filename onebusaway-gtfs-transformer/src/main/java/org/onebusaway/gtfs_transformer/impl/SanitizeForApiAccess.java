512a8eb97e8a0e4ede6a3f91f0f028a51dbf21d9
CaylaSavitzky <41533582+CaylaSavitzky@users.noreply.github.com>
2019-09-16 09:01:42 -0400

    Update SanitizeForApiAccess.java

--- onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/SanitizeForApiAccess.java
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/SanitizeForApiAccess.java
@@ -24,7 +24,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Collection;
 
 public class SanitizeForApiAccess implements GtfsTransformStrategy {
-    private final Logger _log = LoggerFactory.getLogger(SanitizeTripIdForApiAccess.class);
+    private final Logger _log = LoggerFactory.getLogger(SanitizeForApiAccess.class);
 
     private String identitybean;
 
@@ -38,40 +38,28 @@ public class SanitizeForApiAccess implements GtfsTransformStrategy {
 
     @Override
     public void run(TransformContext context, GtfsMutableRelationalDao dao) {
+
+        Collection<?> beans =  dao.getAllTrips();
+
         if (identitybean.equalsIgnoreCase("trip")) {
             _log.info("Removing characters from "  + identitybean + " using this regex: " + regex);
-            Collection<Trip> trips = dao.getAllTrips();
-            for (Trip trip : trips) {
-                String newId = trip.getId().getId().replaceAll(regex, "");
-                if (!newId.equals(trip.getId().getId())) {
-                    trip.getId().setId(newId);
-                }
-            }
+            beans = dao.getAllTrips();
         }
         else if (identitybean.equalsIgnoreCase("stop")) {
             _log.info("Removing characters from "  + identitybean + " using this regex: " + regex);
-            Collection<Stop> stops = dao.getAllStops();
-            for (Stop stop : stops) {
-                String newId = stop.getId().getId().replaceAll(regex, "");
-                if (!newId.equals(stop.getId().getId())) {
-                    stop.getId().setId(newId);
-                }
-            }
+            beans = dao.getAllStops();
         }
         else if (identitybean.equalsIgnoreCase("route") ){
             _log.info("Removing characters from "  + identitybean + " using this regex: " + regex);
-            Collection<Route> routes = dao.getAllRoutes();
-            for (Route route : routes) {
-                String newId = route.getId().getId().replaceAll(regex, "");
-                if (!newId.equals(route.getId().getId())) {
-                    route.getId().setId(newId);
-                }
-            }
+            beans = dao.getAllRoutes();
         }
         else{
             _log.error("No matching Bean Type "+identitybean);
+            return;
         }
+
     }
+
 
     public void setIdentitybean(String identitybean){
         this.identitybean = identitybean;

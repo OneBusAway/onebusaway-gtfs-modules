1b452db35680e0229cffe8d2cfe91bcaa83efa9f
caylasavitzky <caysavitzky@gmail.com>
2019-09-26 08:19:29 -0400

    Logs to info when transit time is greater than one hour Logs error, sns notifies, and delete trips with transit between two stops of greater than one hour Logs error and sns notifies, a collected message about the sum set of trips with transit times greater than one hour Logs error and sns notifies, a message about the sum set of trips with transit times greater than three hours

--- onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/CheckForPlausibleStopTimes.java
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/CheckForPlausibleStopTimes.java
@@ -23,7 +23,7 @@ import org.onebusaway.gtfs_transformer.services.GtfsTransformStrategy;
 import org.onebusaway.gtfs_transformer.services.TransformContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Date;
+import java.util.*;
 
 import java.text.SimpleDateFormat;
 
@@ -41,12 +41,20 @@ public class CheckForPlausibleStopTimes implements GtfsTransformStrategy {
     @Override
     public void run(TransformContext context, GtfsMutableRelationalDao dao) {
         ExternalServices es =  new ExternalServicesBridgeFactory().getExternalServices();
+        RemoveEntityLibrary removeEntityLibrary = new RemoveEntityLibrary();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        Set<Trip> stopsWarn = new HashSet<Trip>();
+        Set<Trip> stopsRemove = new HashSet<Trip>();
+        String collectedWarnString = "";
+        String collectedRemoveString = "";
+
+
 
         for (Trip trip: dao.getAllTrips())
         {
             StopTime oldTime = new StopTime();
             boolean starting = true;
+            stopLoop:
             for (StopTime newTime: dao.getStopTimesForTrip(trip))
             {
                 if (starting)
@@ -55,20 +63,54 @@ public class CheckForPlausibleStopTimes implements GtfsTransformStrategy {
                     oldTime = newTime;
                 }
                 //check if the bus takes more than five hours between stops
-                if(newTime.getArrivalTime() - oldTime.getDepartureTime() > 5 * MINUTES_PER_HOUR * SECONDS_PER_MINUTE){
+                int timeDelta = newTime.getArrivalTime() - oldTime.getDepartureTime();
+
+                if(timeDelta > 1 * MINUTES_PER_HOUR * SECONDS_PER_MINUTE){
                     Date departure = new Date(oldTime.getDepartureTime()*1000);
                     Date arrival = new Date(newTime.getArrivalTime()*1000);
                     String message = "Trip " + trip.getId().getId() + " on Route "+ trip.getRoute().getId() +
-                            " is scheduled for unrealistic transit time when traveling between stoptime" +
+                            " is scheduled for unrealistic transit time (>1hr) when traveling between stoptime" +
                             oldTime.getId()+ " at " + sdf.format(departure) + ", and stoptime" +
                             newTime.getId() + " at " + sdf.format(arrival);
+                    _log.info(message);
+                    stopsWarn.add(trip);
+                    collectedWarnString += ", " + trip.toString() + "at " + sdf.format(departure) +
+                            "and " + sdf.format(arrival);
+                }
+                if(timeDelta > 3 * MINUTES_PER_HOUR * SECONDS_PER_MINUTE){
+                    Date departure = new Date(oldTime.getDepartureTime()*1000);
+                    Date arrival = new Date(newTime.getArrivalTime()*1000);
+                    String message = "Trip " + trip.getId().getId() + " on Route "+ trip.getRoute().getId() +
+                            " is scheduled for unrealistic transit time (>3hr) when traveling between stoptime" +
+                            oldTime.getId()+ " at " + sdf.format(departure) + ", and stoptime" +
+                            newTime.getId() + " at " + sdf.format(arrival) + ". This trip will be deleted.";
+                    _log.error(message);
                     es.publishMessage(getTopic(), message);
-
+                    collectedRemoveString += ", " + trip.toString();
+                    stopsRemove.add(trip);
+                    break stopLoop;
                 }
                 oldTime= newTime;
             }
         }
+
+        collectedWarnString = "Total number of trips with transit times of greater than one hour: " +
+                stopsWarn.size() + ".\n Here are the trips and stops: " + collectedWarnString.substring(2);
+        collectedRemoveString = "Total number of trips with transit times of greater than three hours: " +
+                stopsRemove.size() + ".\n These trips are being removed. \nTrips being removed: " +
+                collectedRemoveString.substring(2);
+
+        _log.error(collectedWarnString);
+        es.publishMessage(getTopic(), collectedWarnString);
+
+        _log.error(collectedRemoveString);
+        es.publishMessage(getTopic(), collectedRemoveString);
+
+        for (Trip trip: stopsRemove){
+            removeEntityLibrary.removeTrip(dao, trip);
+        }
     }
+
 
     private String getTopic() {
         return System.getProperty("sns.topic");

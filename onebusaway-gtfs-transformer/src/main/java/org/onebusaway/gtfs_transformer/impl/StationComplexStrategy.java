228b9038a2379650c81bafcf5b8bcca5f22a2cd4
Jeff Maki <hello@jeffmaki.com>
2020-12-18 13:46:32 -0500

    Create reverse pathways when linking complexes

--- onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/StationComplexStrategy.java
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/StationComplexStrategy.java
@@ -117,7 +117,7 @@ public class StationComplexStrategy implements GtfsTransformStrategy {
                     if (s != null && s.getParentStation() != null && t != null) {
                         if (!s.equals(t)) {
                             String id = String.format("complex-%s-%s-%s", pathwayType.name(), s.getId().getId(), t.getId().getId());
-                            util.createPathway(s, t, pathwayType.ordinal(), genericPathwayTraversalTime, id, null, false);
+                            util.createPathway(s, t, pathwayType.ordinal(), genericPathwayTraversalTime, id, null);
                         }
                     } else {
                         _log.error("Illegal Stop {}", s);

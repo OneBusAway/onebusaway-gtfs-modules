52f5234002a1f36766862b1e72ed4454f0464c0a
lcaraballo <lennycaraballo@gmail.com>
2024-06-10 19:57:54 -0400

    Removing unneeded field mapping

--- onebusaway-gtfs/src/main/java/org/onebusaway/gtfs/serialization/mappings/IconFieldMappingImpl.java
+++
@@ -1,7 +0,0 @@
-package org.onebusaway.gtfs.serialization.mappings;
-
-public class IconFieldMappingImpl extends EntityFieldMappingImpl{
-    public IconFieldMappingImpl(Class<?> entityType, String csvFieldName, String objFieldName, Class<?> objFieldType, boolean required) {
-        super(entityType, csvFieldName, objFieldName, objFieldType, required);
-    }
-}

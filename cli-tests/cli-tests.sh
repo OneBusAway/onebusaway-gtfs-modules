b7f6089a94900ab667f53a7bfedc50ca1d288c50
Leonard Ehrenfried <mail@leonard.io>
2024-09-24 07:06:42 +0200

    Run CLI tests on GH actions

--- cli-tests/cli-tests.sh
+++ cli-tests/cli-tests.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-mvn clean package -DskipTests -Dmaven.source.skip=true -Dmaven.javadoc.skip=true
+mvn clean package --no-transfer-progress -DskipTests -Dmaven.source.skip=true -Dmaven.javadoc.skip=true
 
 # transformer-cli
 
-cp onebusaway-gtfs-transformer-cli/target/onebusaway-gtfs-transformer-cli.jar ./transformer-cli.jar
+TRANSFORMER_JAR="transformer-cli.jar"
+
+cp onebusaway-gtfs-transformer-cli/target/onebusaway-gtfs-transformer-cli.jar ./${TRANSFORMER_JAR}
 wget https://github.com/google/transit/blob/master/gtfs/spec/en/examples/sample-feed-1.zip?raw=true -O gtfs.zip
 
-java -jar transformer-cli.jar --help
+java -jar ${TRANSFORMER_JAR} --help
 
-java -jar transformer-cli.jar --transform="{'op':'remove','match':{'file':'stops.txt','stop_id':'BEATTY_AIRPORT'}}" gtfs.zip gtfs.transformed.zip
+java -jar ${TRANSFORMER_JAR} --transform="{'op':'remove','match':{'file':'stops.txt','stop_id':'BEATTY_AIRPORT'}}" gtfs.zip gtfs.transformed.zip

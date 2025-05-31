9afa6bdfd0568d60900bd39f89c9ea6d227d6670
Leonard Ehrenfried <mail@leonard.io>
2025-05-31 22:37:30 +0200

    Convert to parameterized tests

--- onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/serialization/mappings/StopTimeFieldMappingFactoryTest.java
+++ onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/serialization/mappings/StopTimeFieldMappingFactoryTest.java
@@ -1,30 +1,16 @@
-/**
- * Copyright (C) 2011 Geno Roupsky <geno@masconsult.eu>
- * Copyright (C) 2013 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.onebusaway.gtfs.serialization.mappings;
 
-import static  org.junit.jupiter.api.Assertions.assertEquals;
-import static  org.junit.jupiter.api.Assertions.fail;
-import static org.onebusaway.gtfs.serialization.mappings.StopTimeFieldMappingFactory.getStringAsSeconds;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.onebusaway.gtfs.serialization.mappings.StopTimeFieldMappingFactory.getSecondsAsString;
+import static org.onebusaway.gtfs.serialization.mappings.StopTimeFieldMappingFactory.getStringAsSeconds;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.onebusaway.csv_entities.CsvEntityContext;
 import org.onebusaway.csv_entities.CsvEntityContextImpl;
 import org.onebusaway.csv_entities.schema.BeanWrapper;
@@ -32,92 +18,92 @@ import org.onebusaway.csv_entities.schema.BeanWrapperFactory;
 import org.onebusaway.csv_entities.schema.DefaultEntitySchemaFactory;
 import org.onebusaway.csv_entities.schema.FieldMapping;
 
-public class StopTimeFieldMappingFactoryTest {
+class StopTimeFieldMappingFactoryTest {
 
-  @Test
-  public void test() {
+  private static Stream<Arguments> csvMappingProvider() {
+    return Stream.of(
+            Arguments.of("1234:23:32", new Integer(1234 * 60 * 60 + 23 * 60 + 32))
+    );
+  }
 
+  @ParameterizedTest
+  @MethodSource("csvMappingProvider")
+  void testCsvMapping(String timeStr, Integer expectedSeconds) {
     StopTimeFieldMappingFactory factory = new StopTimeFieldMappingFactory();
     DefaultEntitySchemaFactory schemaFactory = new DefaultEntitySchemaFactory();
     String propName = "time";
-    FieldMapping mapping = factory.createFieldMapping(schemaFactory,
-        Dummy.class, propName, propName, Integer.class, true);
+    FieldMapping mapping = factory.createFieldMapping(
+            schemaFactory,
+            Dummy.class, propName, propName, Integer.class, true
+    );
 
     CsvEntityContext context = new CsvEntityContextImpl();
 
-    Map<String, Object> csvValues = new HashMap<String, Object>();
-    csvValues.put(propName, "1234:23:32");
+    Map<String, Object> csvValues = new HashMap<>();
+    csvValues.put(propName, timeStr);
 
     Dummy obj = new Dummy();
     BeanWrapper wrapped = BeanWrapperFactory.wrap(obj);
 
     mapping.translateFromCSVToObject(context, csvValues, wrapped);
 
-    assertEquals(new Integer(1234 * 60 * 60 + 23 * 60 + 32), obj.getTime());
+    assertEquals(expectedSeconds, obj.getTime());
 
     csvValues.clear();
     mapping.translateFromObjectToCSV(context, wrapped, csvValues);
-    assertEquals("1234:23:32", csvValues.get(propName));
+    assertEquals(timeStr, csvValues.get(propName));
   }
 
-  @Test
-  public void testGetStringAsSeconds() {
-    assertEquals(0, getStringAsSeconds("00:00:00"));
-    assertEquals(0, getStringAsSeconds("-00:00:00"));
-
-    assertEquals(60, getStringAsSeconds("00:01:00"));
-    assertEquals(60, getStringAsSeconds("-00:01:00"));
-
-    assertEquals(3660, getStringAsSeconds("01:01:00"));
-    assertEquals(3660, getStringAsSeconds("1:01:00"));
-    assertEquals(-3540, getStringAsSeconds("-01:01:00"));
-
-    assertEquals(37230, getStringAsSeconds("10:20:30"));
-    assertEquals(-34770, getStringAsSeconds("-10:20:30"));
-
-    assertEquals(360913, getStringAsSeconds("100:15:13"));
-    assertEquals(-359087, getStringAsSeconds("-100:15:13"));
-
-    try {
-      getStringAsSeconds("");
-      fail();
-    } catch (InvalidStopTimeException ex) {
-
-    }
-
-    try {
-      getStringAsSeconds("000000");
-      fail();
-    } catch (InvalidStopTimeException ex) {
-
-    }
-
-    try {
-      getStringAsSeconds("00:00");
-      fail();
-    } catch (InvalidStopTimeException ex) {
-
-    }
-
-    try {
-      getStringAsSeconds("--00:00:00");
-      fail();
-    } catch (InvalidStopTimeException ex) {
-
-    }
+  private static Stream<Arguments> stringSecondsProvider() {
+    return Stream.of(
+            Arguments.of("00:00:00", 0),
+            Arguments.of("-00:00:00", 0),
+            Arguments.of("00:01:00", 60),
+            Arguments.of("-00:01:00", 60),
+            Arguments.of("01:01:00", 3660),
+            Arguments.of("1:01:00", 3660),
+            Arguments.of("-01:01:00", -3540),
+            Arguments.of("10:20:30", 37230),
+            Arguments.of("-10:20:30", -34770),
+            Arguments.of("100:15:13", 360913),
+            Arguments.of("-100:15:13", -359087)
+    );
   }
 
-  @Test
-  public void getGetSecondsAsString() {
-    assertEquals("00:00:00", getSecondsAsString(0));
-    assertEquals("00:01:00", getSecondsAsString(60));
-    assertEquals("-01:59:00", getSecondsAsString(-60));
-    assertEquals("01:01:00", getSecondsAsString(3660));
-    assertEquals("-01:01:00", getSecondsAsString(-3540));
-    assertEquals("10:20:30", getSecondsAsString(37230));
-    assertEquals("-10:20:30", getSecondsAsString(-34770));
-    assertEquals("100:15:13", getSecondsAsString(360913));
-    assertEquals("-100:15:13", getSecondsAsString(-359087));
+  @ParameterizedTest
+  @MethodSource("stringSecondsProvider")
+  void testGetStringAsSeconds(String input, int expected) {
+    assertEquals(expected, getStringAsSeconds(input));
+  }
+
+  private static Stream<String> invalidTimeStringsProvider() {
+    return Stream.of("", "000000", "00:00", "--00:00:00");
+  }
+
+  @ParameterizedTest
+  @MethodSource("invalidTimeStringsProvider")
+  void testInvalidTimesThrowException(String input) {
+      assertThrows(InvalidStopTimeException.class, () -> getStringAsSeconds(input));
+  }
+
+  private static Stream<Arguments> secondsStringProvider() {
+    return Stream.of(
+            Arguments.of(0, "00:00:00"),
+            Arguments.of(60, "00:01:00"),
+            Arguments.of(-60, "-01:59:00"),
+            Arguments.of(3660, "01:01:00"),
+            Arguments.of(-3540, "-01:01:00"),
+            Arguments.of(37230, "10:20:30"),
+            Arguments.of(-34770, "-10:20:30"),
+            Arguments.of(360913, "100:15:13"),
+            Arguments.of(-359087, "-100:15:13")
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("secondsStringProvider")
+  void testGetSecondsAsString(int input, String expected) {
+    assertEquals(expected, getSecondsAsString(input));
   }
 
   public static class Dummy {

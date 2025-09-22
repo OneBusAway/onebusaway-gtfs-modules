63b6964bb2924ce9908d2ade18358e6371ba40b1
Leonard Ehrenfried <mail@leonard.io>
2025-09-22 11:00:41 +0200

    Add DAO methods

--- onebusaway-gtfs/src/main/java/org/onebusaway/gtfs/model/RouteNetworkAssignment.java
+++ onebusaway-gtfs/src/main/java/org/onebusaway/gtfs/model/RouteNetworkAssignment.java
@@ -2,22 +2,23 @@ package org.onebusaway.gtfs.model;
 
 import org.onebusaway.csv_entities.schema.annotations.CsvField;
 import org.onebusaway.csv_entities.schema.annotations.CsvFields;
+import org.onebusaway.gtfs.serialization.mappings.EntityFieldMappingFactory;
 
 @CsvFields(filename = "route_networks.txt")
 public final class RouteNetworkAssignment extends IdentityBean<String> {
 
-  @CsvField(name = "route_id")
-  private String routeId;
+  @CsvField(name = "route_id", optional = true, mapping = EntityFieldMappingFactory.class)
+  private Route route;
 
   @CsvField(name = "network_id")
   private String networkId;
 
-  private String getRouteId() {
-    return routeId;
+  private Route getRouteId() {
+    return route;
   }
 
-  private void setRouteId(String routeId) {
-    this.routeId = routeId;
+  private void setRouteId(Route routeId) {
+    this.route = routeId;
   }
 
   private String getNetworkId() {
@@ -29,7 +30,7 @@ public final class RouteNetworkAssignment extends IdentityBean<String> {
   }
 
   public String getId() {
-    return routeId + "|" + networkId;
+    return route.getId() + "|" + networkId;
   }
 
   @Override

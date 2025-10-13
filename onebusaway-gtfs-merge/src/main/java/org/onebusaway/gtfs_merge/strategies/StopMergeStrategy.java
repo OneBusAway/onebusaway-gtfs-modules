9afa22d43feb98ef7d6afd6b0b3ce82af5a014df
Leonard Ehrenfried <mail@leonard.io>
2025-10-13 13:47:26 +0200

    Update docs

--- onebusaway-gtfs-merge/src/main/java/org/onebusaway/gtfs_merge/strategies/StopMergeStrategy.java
+++ onebusaway-gtfs-merge/src/main/java/org/onebusaway/gtfs_merge/strategies/StopMergeStrategy.java
@@ -13,10 +13,8 @@
  */
 package org.onebusaway.gtfs_merge.strategies;
 
-import org.onebusaway.gtfs.model.IdentityBean;
 import org.onebusaway.gtfs.model.Stop;
 import org.onebusaway.gtfs.model.StopTime;
-import org.onebusaway.gtfs.services.GtfsMutableRelationalDao;
 import org.onebusaway.gtfs.services.GtfsRelationalDao;
 import org.onebusaway.gtfs_merge.GtfsMergeContext;
 import org.onebusaway.gtfs_merge.strategies.scoring.StopDistanceDuplicateScoringStrategy;
@@ -44,13 +42,5 @@ public class StopMergeStrategy extends AbstractIdentifiableSingleEntityMergeStra
         source.getAllTransfers(), oldStop, newStop, "fromStop", "toStop");
     MergeSupport.bulkReplaceValueInProperties(
         source.getAllPathways(), oldStop, newStop, "fromStop", "toStop");
-  }
-
-  @Override
-  protected void save(GtfsMergeContext context, IdentityBean<?> entity) {
-    GtfsRelationalDao source = context.getSource();
-    GtfsMutableRelationalDao target = context.getTarget();
-
-    super.save(context, entity);
   }
 }
